45a12221ea731605286748c3fd32b1fb2455621c
Armin Balalaie <armin.balalaie@gmail.com>
2017-03-17 17:39:51 -0700

    pagerank bugs has been fixed

--- src/pagerank/pagerank.py
+++ src/pagerank/pagerank.py
@@ -7,19 +7,38 @@ import requests
 from w3lib.url import canonicalize_url
 from constants import URL_FILE, RAW_DATA_BASE_PATH, PAGERANK_FILE
 import networkx as nx
+from lxml import html
 
 from indexing.html_parser import unpack_document_id
 
 
-def load_url_dict():
+def load_file_to_url_dict():
     with open(URL_FILE) as f:
-        return json.load(f)
+        temp_dict = json.load(f)
+    file_to_url = dict()
+    for key, val in temp_dict.items():
+        file_to_url[key] = cleanup_url(val)
+    return file_to_url
 
+def load_url_to_file_dict():
+    with open(URL_FILE) as f:
+        temp_dict = json.load(f)
+    url_to_file = dict()
+    for key, val in temp_dict.items():
+        url_to_file[cleanup_url(val)] = key
+    return url_to_file
+
+def cleanup_url(url):
+    parsed = urlparse(url)
+    url = parsed.netloc + parsed.path + "?" + parsed.query
+    url = canonicalize_url(url)
+    if url[len(url) - 1] == '/':
+        url = url[:len(url) - 1]
+    return url
 
 def save_pagerank(pr):
     with open(PAGERANK_FILE, 'wb') as f:
         pickle.dump(pr, f, pickle.HIGHEST_PROTOCOL)
-
 
 def load_pagerank():
     with open(PAGERANK_FILE, 'r') as f:
@@ -27,17 +46,13 @@ def load_pagerank():
 
 
 if __name__ == "__main__":
-    file_to_url_dict = load_url_dict()
-    url_to_file_dict = dict()
+    file_to_url_dict = load_file_to_url_dict()
+    url_to_file_dict = load_url_to_file_dict()
     G = nx.DiGraph()
     count = 0
 
-    for key, val in file_to_url_dict.items():
-        url_to_file_dict[val] = key
-
-    for key, val in file_to_url_dict.items():
-        # resp = requests.get(val)
-        folder_name, file_name = unpack_document_id(key)
+    for file, url in file_to_url_dict.items():
+        folder_name, file_name = unpack_document_id(file)
         count += 1
         # if folder_name == "0" and int(file_name) < 50:
         data_file = None
@@ -48,25 +63,49 @@ if __name__ == "__main__":
             # print "Not found", folder_name,'/', file_name
             continue
 
+        if file not in G.nodes():
+            G.add_node(file)
+
         if count%1000 == 0:
             print count
-        # encoding = resp.encoding if 'charset' in resp.headers.get('content-type', '').lower() else None
-        soup = BeautifulSoup(data, "html.parser")
-        if soup.find('html') is not None:
-            if key not in G.nodes():
-                G.add_node(key)
-            for link in soup.find_all('a', href=True):
-                parsed = link.get('href',None)
-                parsed = urlparse(parsed)
-                url = parsed.netloc + parsed.path + "?" + parsed.query
-                url = canonicalize_url(url)
-                if url[len(url) - 1] == '/':
-                    url = url[:len(url) - 1]
-                if url != val:
-                    if url_to_file_dict.has_key(url):
-                        if url_to_file_dict[url] not in G.nodes():
-                            G.add_node(url_to_file_dict[url])
-                        G.add_edge(key, url_to_file_dict[url])
+
+        # Parsing HTML content to extract links
+        try:
+            tree = html.fromstring(data)
+            raw_links = tree.xpath('//a/@href')
+        except Exception:
+            continue
+
+        default_scheme = urlparse(url).scheme
+
+        for link in raw_links:
+            if len(link) < 2: continue
+            # Make absolute links
+            parsed = urlparse(link)
+            if parsed.scheme not in ["","http", "https"]:
+                continue
+            if link[0] is "/":
+                if link[1] is "/": # starts with //
+                    absolute_url = default_scheme + ":" + link
+                else: # starts with /
+                    absolute_url = default_scheme + "://" + urlparse(url).netloc + link
+            elif parsed.netloc is "":
+                rslash_pos = url.rfind("/")
+                if rslash_pos is -1:
+                    absolute_url = url + "/" + link
+                else:
+                    absolute_url = url[:rslash_pos+1] + link
+            else:
+                absolute_url = link
+
+            absolute_url = cleanup_url(absolute_url)
+
+            if url != absolute_url:
+                if url_to_file_dict.has_key(absolute_url):
+                    if url_to_file_dict[absolute_url] not in G.nodes():
+                        G.add_node(url_to_file_dict[absolute_url])
+                    G.add_edge(file, url_to_file_dict[absolute_url])
+
     pr = nx.pagerank(G, alpha=0.9)
     save_pagerank(pr)
 
